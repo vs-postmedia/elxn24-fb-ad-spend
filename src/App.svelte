3d2728ade44bcc69e9a24372052aaca670e18e6e
Nathan Griffiths <ngriffiths@postmedia.com>
2024-10-01 17:52:09 -0700

    copy edit

--- src/App.svelte
+++ src/App.svelte
@@ -28,7 +28,7 @@
 </script>
 
 <header>
-    <h1>Ad spending for B.C.’s 2024 election on Meta</h1>
+    <h1>Ad spending on Meta in B.C.’s 2024 election</h1>
     <!-- <p class="subhead">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 </header>
 
